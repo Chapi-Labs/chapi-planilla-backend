ad7ba5365b900bfa777e8eb04eb9c486618ab847
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-04-14 13:30:06 -0600

    update employee

--- src/resolvers/mutations/EmployeeMutations.js
+++ src/resolvers/mutations/EmployeeMutations.js
@@ -7,15 +7,34 @@ const EmployeeMutations = {
     }
     // 2. create Employee
     const employee = await ctx.db.mutation.createEmployee(
-        {
-            data: {
-                ...args,
-                active: true
-            }
-        },
-        info
+      {
+        data: {
+          ...args,
+          active: true
+        }
+      },
+      info
     );
     return employee;
+  },
+  async updateEmployee(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateEmployee(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
