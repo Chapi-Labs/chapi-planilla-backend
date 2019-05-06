7979d23e1b25717c59e268247e8ed9155f4c93a3
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-05-05 23:26:33 -0600

    save new employee

--- src/resolvers/mutations/EmployeeMutations.js
+++ src/resolvers/mutations/EmployeeMutations.js
@@ -1,17 +1,44 @@
-
 const EmployeeMutations = {
   async createEmployee(parent, args, ctx, info) {
     // 1. Check if they are logged in
     if (!ctx.request.userId) {
       throw new Error('You must be logged in!');
     }
+    const frequencyPromise = ctx.db.query.payrollConfig(
+      {
+        where: { id: args.payroll_frequency }
+      },
+      `{ id name frequency }`
+    );
+
+    const companyPromise = ctx.db.query.company(
+      {
+        where: { id: args.company }
+      },
+      `{ id active name }`
+    );
+    const [frequency, company] = await Promise.all([
+      frequencyPromise,
+      companyPromise
+    ]);
+    const idConfig = frequency.id;
+    const idCompany = company.id;
+    delete frequency.id;
+    delete company.id;
     // 2. create Employee
     const employee = await ctx.db.mutation.createEmployee(
       {
         data: {
           ...args,
           active: true,
-          createdBy: { connect: { id: ctx.request.userId }}
+          createdBy: { connect: { id: ctx.request.userId } },
+          payroll_frequency: {
+            create: {
+              ...frequency,
+              _id: idConfig
+            }
+          },
+          company: { create: { ...company, _id: idCompany } }
         }
       },
       info
