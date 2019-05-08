38a680ba3a9e10475e7a58d1f98f39064f892b7d
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-05-07 23:20:59 -0600

    add validations on backend

--- src/resolvers/mutations/EmployeeMutations.js
+++ src/resolvers/mutations/EmployeeMutations.js
@@ -4,13 +4,21 @@ const EmployeeMutations = {
     if (!ctx.request.userId) {
       throw new Error('You must be logged in!');
     }
+    const findEmployee = await ctx.db.query.employee({
+      where: { email: args.email }
+    }, `{ id }`);
+    if (findEmployee) {
+      throw new Error('El empleado ya existe');
+    }
+    if (args.payroll_frequency == null || args.company == null) {
+      throw new Error('Formulario incompleto')
+    }
     const frequencyPromise = ctx.db.query.payrollConfig(
       {
         where: { id: args.payroll_frequency }
       },
       `{ id name frequency }`
     );
-
     const companyPromise = ctx.db.query.company(
       {
         where: { id: args.company }
@@ -21,10 +29,28 @@ const EmployeeMutations = {
       frequencyPromise,
       companyPromise
     ]);
-    const idConfig = frequency.id;
-    const idCompany = company.id;
-    delete frequency.id;
-    delete company.id;
+    let relation_obj = {};
+    if (frequency != null) {
+      const idConfig = frequency.id;
+      delete frequency.id;
+      relation_obj = {
+        ...relation_obj,
+        payroll_frequency: {
+          create: {
+            ...frequency,
+            _id: idConfig
+          }
+        }
+      }
+    }
+    if (company != null) {
+      const idCompany = company.id;
+      delete company.id;
+      relation_obj = {
+        ...relation_obj,
+        company: { create: { ...company, _id: idCompany } }
+      };
+    }
     // 2. create Employee
     const employee = await ctx.db.mutation.createEmployee(
       {
@@ -32,13 +58,7 @@ const EmployeeMutations = {
           ...args,
           active: true,
           createdBy: { connect: { id: ctx.request.userId } },
-          payroll_frequency: {
-            create: {
-              ...frequency,
-              _id: idConfig
-            }
-          },
-          company: { create: { ...company, _id: idCompany } }
+          ...relation_obj
         }
       },
       info
